3fa3f1b141d14eb01418fc8f57b70bf9941c3613
n1kutochkin <n1kutochkin.personal@gmail.com>
2022-05-10 18:57:53 +0300

    completed l3 ex1 & 2

--- library/Lib.hs
+++ library/Lib.hs
@@ -2,16 +2,23 @@ module Lib where
 
 -- "Наивный" рекурсивный вариант
 fibN :: Integer -> Integer
-fibN n = undefined
+fibN 0 = 1
+fibN 1 = 1
+fibN n = fibN (n-1) + fibN (n-2)
 
 -- Вариант, использующий хвостовую рекурсию
 fibNTailRecursive :: Integer -> Integer
-fibNTailRecursive = undefined
+fibNTailRecursive x = _fib x 1 1
+
+_fib :: Integer -> Integer -> Integer -> Integer
+_fib n ppre pre | n <= 1 = ppre
+_fib n ppre pre = _fib (n-1) pre (ppre + pre)
 
 -- "Наивная" реализация на основе fibNTailRecursive и map
 fibNaive :: [Integer]
-fibNaive = undefined
+fibNaive = map fibNTailRecursive [1..]
 
 -- Рекурсивное определение с использованием zipWith
 fib :: [Integer]
-fib =  undefined
+fib = 0 : _next
+    where _next = 1 : zipWith (+) fib _next
